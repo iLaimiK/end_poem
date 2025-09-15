ca657bbf34c1c5062b56d078597c3f232e5aae8e
iLaimiK <1013069090@qq.com>
2025-09-15 21:53:22 +0800

    update template

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,vue,js,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
