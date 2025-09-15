fdac27680edce85381b66c1a07d4ad4497519a6c
iLaimiK <1013069090@qq.com>
2025-09-15 22:47:37 +0800

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -69,7 +69,6 @@ export default [
       'prefer-const': 'warn',
       'vue/multi-word-component-names': 'off',
       yoda: 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/no-unused-vars': 'off',
     },
   },
